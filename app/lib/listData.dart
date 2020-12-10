64a76bba8e16692a9a69a845f79b714290d3130a
yasmin537 <52938483+yasmin537@users.noreply.github.com>
2020-12-10 15:50:25 -0700

    finished some pages

--- app/lib/listData.dart
+++ app/lib/listData.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/animation.dart';
+
 
 class Restaurant {
   final String name;
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:app/listData.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
               // is not restarted.
               primarySwatch: Colors.teal,
             ),
-            home: MyHomePage()
+            home: MyHomePage(),//title: Text('Favourites')),
 
     );
   }
@@ -36,7 +37,8 @@ class MyApp extends StatelessWidget {
    Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Served'),
+
+        title: Text('Favourites'),
         actions: <Widget>[
           IconButton(
             onPressed: (){
@@ -45,28 +47,66 @@ class MyApp extends StatelessWidget {
           ),
         ],
       ),
+      body: Column(
+          children: <Widget> [
+         FlatButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(
+                  MaterialPageRoute(
+                    builder: (context) => Restaurant1()
+                  )
+              );
+            },
+           child: Text('Restaurant 1', style: TextStyle(fontSize: 25)),
+          ),
+            Divider(),
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(
+                    MaterialPageRoute(
+                        builder: (context) => Restaurant2()
+                    )
+                );
+              },
+              child: Text('Restaurant 2', style: TextStyle(fontSize: 25)),
+            ),
+            Divider(),
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(
+                    MaterialPageRoute(
+                        builder: (context) => Restaurant3()
+                    )
+                );
+              },
+              child: Text('Restaurant 3', style: TextStyle(fontSize: 25)),
+            ),
+            Divider(),
+            FlatButton(
+              onPressed: () {Navigator.of(context)
+                  .push(
+                  MaterialPageRoute(
+                      builder: (context) => Restaurant4()
+                  )
+              );},
+              child: Text('Restaurant 4', style: TextStyle(fontSize: 25)),
+            ),
+            Divider(),
+          ],
+      ),
+
     );
   }
 
-   // This widget is the home page of your application. It is stateful, meaning
-   // that it has a State object (defined below) that contains fields that affect
-   // how it looks.
 
-   // This class is the configuration for the state. It holds the values (in this
-   // case the title) provided by the parent (in this case the App widget) and
-   // used by the build method of the State. Fields in a Widget subclass are
-   // always marked "final".
-
-   //final String title;
-
-   //@override
-   //_MyHomePageState createState() => _MyHomePageState();
  }
 //This class is responsible for searching restaurant items
  class restSearch extends SearchDelegate<Restaurant> {
   @override
   List<Widget> buildActions(BuildContext context) {
-    // TODO: implement buildActions
    return [IconButton(icon: Icon(Icons.clear), onPressed: (){
      query ="";
    },)];
@@ -74,19 +114,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
     return IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back),);
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
     return Center(child: Text(query,style: TextStyle(fontSize: 20),));
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
     final myList = query.isEmpty? loadRestaurants()
     : loadRestaurants().where((p) => p.name.startsWith(query)).toList();
     return myList.isEmpty? Text('No Results Found') : ListView.builder(
@@ -109,69 +146,98 @@ class MyApp extends StatelessWidget {
   
  }
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
+ //This page is dedicated to the first restaurant listed in the home page
+ class Restaurant1 extends StatelessWidget{
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(title: Text('Restaurant 1')),
+       body:
+           Column (children: <Widget>[
+             Text('Description',style: TextStyle(fontSize: 30, height: 1.5),),
+             Text('Description goes here',style: TextStyle(
+                 fontSize: 20, height: 2.0),),
+             Divider(),
+             Text('Menu',style: TextStyle(fontSize: 30),),
+             Text('Appetizers',style: TextStyle(
+                 fontSize: 25, height: 2.0,),),
+             Text('Main Course',style: TextStyle(
+               fontSize: 25, height: 2.0,),),
+             Text('Desserts',style: TextStyle(
+               fontSize: 25, height: 2.0,),),
+           ])
+     );
+   }
+ }
+
+//This page is dedicated to the second restaurant listed in the home page
+class Restaurant2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Restaurant 2')),
+        body:
+        Column (children: <Widget>[
+          Text('Description',style: TextStyle(fontSize: 30, height: 1.5),),
+          Text('Description goes here',style: TextStyle(
+              fontSize: 20, height: 2.0),),
+          Divider(),
+          Text('Menu',style: TextStyle(fontSize: 30),),
+          Text('Appetizers',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+          Text('Main Course',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+          Text('Desserts',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+        ])
+    );
+  }
+}
+
+//This page is dedicated to the third restaurant listed in the home page
+class Restaurant3 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Restaurant 3')),
+        body:
+        Column (children: <Widget>[
+          Text('Description',style: TextStyle(fontSize: 30, height: 1.5),),
+          Text('Description goes here',style: TextStyle(
+              fontSize: 20, height: 2.0),),
+          Divider(),
+          Text('Menu',style: TextStyle(fontSize: 30),),
+          Text('Appetizers',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+          Text('Main Course',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+          Text('Desserts',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+        ])
+    );
+  }
+}
+
+//This page is dedicated to the fourth restaurant listed in the home page
+class Restaurant4 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Restaurant 4')),
+        body:
+        Column (children: <Widget>[
+          Text('Description',style: TextStyle(fontSize: 30, height: 1.5),),
+          Text('Description goes here',style: TextStyle(
+              fontSize: 20, height: 2.0),),
+          Divider(),
+          Text('Menu',style: TextStyle(fontSize: 30),),
+          Text('Appetizers',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+          Text('Main Course',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+          Text('Desserts',style: TextStyle(
+            fontSize: 25, height: 2.0,),),
+        ])
+    );
+  }
+}
